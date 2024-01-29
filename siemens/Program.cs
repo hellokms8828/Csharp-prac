b4c62217094917776808c5cf406c8d4349670479
hellokms8828 <hellokms8828@naver.com>
2024-01-29 23:21:05 +0900

    First commit

---
+++ siemens/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace siemens
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
